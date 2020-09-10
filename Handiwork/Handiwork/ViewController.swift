79bd9d209e2f9d19966e166630d007a8a6414f2d
CoreyBurgin <corey.burgin22@gmail.com>
2020-09-10 08:56:13 -0400

    Initial Project setup
    Implemented Canvas View that will track your fingers through touchesBegan and touchsMoved

---
+++ Handiwork/Handiwork/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  Handiwork
+//
+//  Created by Corey Burgin on 9/8/20.
+//  Copyright © 2020 Corey Burgin. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+class Canvas: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+//        
+//        let startPoint = CGPoint(x: 0, y: 0)
+//        let endPoint = CGPoint(x: 100, y: 100)
+//        
+        
+        
+    
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setLineWidth(10)
+        context.setLineCap(.butt)
+        
+        
+        
+        lines.forEach { (line) in
+            for (i, p) in line.enumerated() {
+                if i == 0 {
+                    context.move(to: p)
+                } else {
+                    context.addLine(to: p)
+                }
+            }
+        }
+        
+        context.strokePath()
+    }
+    
+    var lines = [[CGPoint]]()
+    //tracks Finger as we move across screen
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
+    }
+    
+    
+    
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard let point = touches.first?.location(in: nil) else { return }
+        
+        guard var lastLine = lines.popLast() else  { return }
+        
+        lastLine.append(point)
+        lines.append(lastLine)
+        
+        setNeedsDisplay()
+    }
+    
+    
+}
+
+class ViewController: UIViewController {
+    
+    let canvas = Canvas()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.addSubview(canvas)
+        canvas.frame = view.frame
+        canvas.backgroundColor = .white
+        view.backgroundColor = .blue
+    }
+
+
+}
+
