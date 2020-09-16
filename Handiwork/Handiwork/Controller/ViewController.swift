60d41393232896e55082208f2d90309c7dbcced4
CoreyBurgin <corey.burgin22@gmail.com>
2020-09-16 19:43:09 -0400

    Adding More Features
    dded a new model layer along with a stackview of uibuttions and a UIslider that changes the width of the stroke and color

--- Handiwork/Handiwork/Controller/ViewController.swift
+++ Handiwork/Handiwork/Controller/ViewController.swift
@@ -8,25 +8,118 @@
 
 import UIKit
 
-
-
-
-
-
 class ViewController: UIViewController {
     
     let canvas = Canvas()
     
-
+    let undoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Undo", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(handleUndo), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc fileprivate func handleUndo() {
+        print("Undo lines drawn")
+        canvas.undo()
+    }
+    
+    let clearButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Clear", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(handleClear), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func handleClear() {
+        canvas.clear()
+    }
+    
+    
+    let yellowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .yellow
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(handleColorChange), for: .touchUpInside)
+        return button
+    }()
+    
+    let redButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .red
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(handleColorChange), for: .touchUpInside)
+        return button
+    }()
+    
+    let blueButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .blue
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(handleColorChange), for: .touchUpInside)
+        return button
+    }()
+   
+    
+    
+    @objc fileprivate func handleColorChange(button: UIButton) {
+        canvas.setStrokeColor(color: button.backgroundColor ?? .black)
+    }
+       
+    
+    
+    let slider: UISlider = {
+        let slider = UISlider()
+        slider.minimumValue = 1
+        slider.maximumValue = 10
+        slider.addTarget(self, action: #selector(handleSliderChange), for: .valueChanged)
+        return slider
+    }()
+    
+    
+    @objc fileprivate func handleSliderChange() {
+        canvas.setStrokeWidth(width: slider.value)
+    }
+    
+    
+    override func loadView() {
+        self.view = canvas
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.addSubview(canvas)
-        canvas.frame = view.frame
-        canvas.backgroundColor = .white
-        view.backgroundColor = .blue
+        
+        canvas.backgroundColor = .black
+        setupLayout()
+    }
+    
+    fileprivate func setupLayout() {
+        let colorStackView = UIStackView(arrangedSubviews: [yellowButton, redButton, blueButton])
+        colorStackView.distribution = .fillEqually
+        
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            undoButton,
+            clearButton,
+            colorStackView,
+            slider
+            ])
+        stackView.spacing = 12
+        stackView.distribution = .fillEqually
+        
+    
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+       
     }
 
-
 }
-
---
+++ Handiwork/Handiwork/Models/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  Handiwork
+//
+//  Created by Corey Burgin on 9/16/20.
+//  Copyright © 2020 Corey Burgin. All rights reserved.
+//
+
+import UIKit
+
+struct Line {
+    let strokeWidth: Float
+    let color: UIColor
+    var points: [CGPoint]
+}
